38082a6c08edf771019bdf7443e30aed9cdf187f
Eduponge <eduardo.pongeluppe@gmail.com>
2025-07-24 21:23:50 -0300

    Update tirar_screenshot.sh

--- scripts/tirar_screenshot.sh
+++ scripts/tirar_screenshot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-LINK="$1"
+LINK="https://app.powerbi.com/view?r=eyJrIjoiY2Q3NDU0ZTYtNzBjNS00NzE5LTkzMzEtMGU3ODRhZDc4YjY5IiwidCI6ImQ4NDI2OWQ4LWMxNWUtNGRmMS1iOWRmLTBlNjAzMWMzZjc0YyJ9"
+TOKEN="7924491565:AAE9HMizTz08P20par9Ij0Gw-7pXVBdb1Xs"
+CHAT_ID="7925545189"
 DATA=$(date +%Y%m%d_%H%M%S)
 ARQUIVO="screenshot_${DATA}.png"
 
+# Gera o screenshot com puppeteer
 node - <<EOF
 const puppeteer = require('puppeteer');
 (async () => {
@@ -13,3 +16,9 @@ const puppeteer = require('puppeteer');
   await browser.close();
 })();
 EOF
+
+# Envia screenshot para o Telegram
+curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendPhoto" \
+  -F chat_id="${CHAT_ID}" \
+  -F photo=@"${ARQUIVO}" \
+  -F caption="Screenshot do Power BI em ${DATA}"
